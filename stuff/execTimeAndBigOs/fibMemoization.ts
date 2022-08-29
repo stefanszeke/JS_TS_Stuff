0d3f31fcc6d6eb571efc684a194785b7801f5ea1
Stefan Szekely <sekestefan@gmail.com>
2022-08-29 09:52:22 +0200

    add: more fibonacci

--- stuff/execTimeAndBigOs/fibMemoization.ts
+++ stuff/execTimeAndBigOs/fibMemoization.ts
@@ -1,6 +1,14 @@
 import {performance} from 'perf_hooks';
 
+// memoization is a technique to speed up the execution of a function by caching the results of previous invocations.
+
+
+
+
 // classic fibonacci
+// with a big O of O(2^n), because it is exponential, it is very slow.
+// in each step, we call the function two time more, then the previous step.
+
 const getFibonacci = (n: number): number => {
   if (n < 2) {
     return n;
@@ -34,6 +42,27 @@ console.log(`fibonacci:  ${end1 - start1}ms`)
 
 // getFibonacci - ~11000 ms
 const start2: number = performance.now();
-console.log(getFibonacci(0));
+console.log(getFibonacci(45));
 const end2: number = performance.now();
 console.log(`fibonacci:  ${end2 - start2}ms`)
+
+
+
+// 
+const fib2 = (n: number, cache: number[] = []): number => {
+  if (n < 2)return n;
+  
+  if (cache[n]) return cache[n];
+  
+  cache[n] = fib2(n - 1, cache) + fib2(n - 2, cache);
+  return cache[n];
+}
+
+const start3: number = performance.now();
+console.log(fib2(45));
+const end3: number = performance.now();
+console.log(`fibonacci:  ${end3 - start3}ms`)
+
+
+
+
